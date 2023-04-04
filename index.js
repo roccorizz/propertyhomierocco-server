41d9adab1f1b26109f68725930449ef541dbbd8e
roccorizz <sknahid68@gmail.com>
2023-04-05 01:33:34 +0600

    fixing bugs

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const cors = require('cors');
 let jwt = require('jsonwebtoken');
 
 const app = express()
+app.use(cors({ origin: 'http://localhost:3001' }));
 const { MongoClient, ObjectId, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 8000;
 // middle wares
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*')
-//     res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE')
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-//     next()
-// })
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
+
 app.use(cors());
 app.use(express.json());
 
